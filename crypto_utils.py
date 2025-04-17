da74cf5fc94d43a5f277600d80d4a970f53d78a0
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-04-17 21:53:16 +1000

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,9 +1,42 @@
 from kyber_py.ml_kem import ML_KEM_1024
+from dilithium_py.ml_dsa import ML_DSA_87
 
-def generate_keys():
+def generate_kem_keys():
     ek, dk = ML_KEM_1024.keygen()
     return ek, dk
 
+def derive_client_kem_key(ek):
+    key, ct = ML_KEM_1024.encaps(ek)
+    return key, ct
+
+def derive_server_key(dk, ct):
+    key = ML_KEM_1024.decaps(dk, ct)
+    return key
+
+def generate_dsa_keys():
+    pk, sk = ML_DSA_87.keygen()
+    return pk, sk
+
+def create_signature(sk, msg):
+    return ML_DSA_87.sign(sk, msg)
+
+def verify_signature(pk, msg, sig):
+    return ML_DSA_87.verify(pk, msg, sig)
 
 if __name__ == "__main__":
-    ek, dk = generate_keys()
+    # SERVER
+    pk, sk = generate_dsa_keys()
+    ek, dk = generate_kem_keys()
+    sig = create_signature(sk, ek)
+    ## SEND PK, EK
+
+    # CLIENT
+    ## OBTAIN PK FROM CERTIFICATE AUTHORITY
+    if verify_signature(pk, ek, sig):
+        client_key, ct = derive_client_kem_key(ek)
+        # SEND CT
+    
+    # SERVER
+    server_key = derive_server_key(dk, ct)
+
+    print(client_key == server_key)
