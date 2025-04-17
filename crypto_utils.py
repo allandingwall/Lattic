9ed22f54c71a2ea8b8b4b3d1eb1d3169df7fa476
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-04-17 21:58:36 +1000

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -2,41 +2,44 @@ from kyber_py.ml_kem import ML_KEM_1024
 from dilithium_py.ml_dsa import ML_DSA_87
 
 def generate_kem_keys():
-    ek, dk = ML_KEM_1024.keygen()
-    return ek, dk
+    encap_key, decap_key = ML_KEM_1024.keygen()
+    return encap_key, decap_key
 
-def derive_client_kem_key(ek):
-    key, ct = ML_KEM_1024.encaps(ek)
-    return key, ct
+def encapsulate_key(encap_key):
+    key, ciphertext = ML_KEM_1024.encaps(encap_key)
+    return key, ciphertext
 
-def derive_server_key(dk, ct):
-    key = ML_KEM_1024.decaps(dk, ct)
+def decapsulate_key(decap_key, ciphertext):
+    key = ML_KEM_1024.decaps(decap_key, ciphertext)
     return key
 
 def generate_dsa_keys():
-    pk, sk = ML_DSA_87.keygen()
-    return pk, sk
+    dsa_pub_key, dsa_sec_key = ML_DSA_87.keygen()
+    return dsa_pub_key, dsa_sec_key
 
-def create_signature(sk, msg):
-    return ML_DSA_87.sign(sk, msg)
+def create_signature(dsa_sec_key, msg):
+    return ML_DSA_87.sign(dsa_sec_key, msg)
 
 def verify_signature(pk, msg, sig):
     return ML_DSA_87.verify(pk, msg, sig)
 
 if __name__ == "__main__":
     # SERVER
-    pk, sk = generate_dsa_keys()
-    ek, dk = generate_kem_keys()
-    sig = create_signature(sk, ek)
-    ## SEND PK, EK
+    dsa_pub_key, dsa_sec_key = generate_dsa_keys()
+    encap_key, decap_key = generate_kem_keys()
+    sig = create_signature(sk, encap_key)
+    ## SEND PK, EK TO CLIENT
 
     # CLIENT
     ## OBTAIN PK FROM CERTIFICATE AUTHORITY
-    if verify_signature(pk, ek, sig):
-        client_key, ct = derive_client_kem_key(ek)
-        # SEND CT
+    if verify_signature(dsa_pub_key, encap_key, sig):
+        client_key, ciphertext = encapsulate_key(encap_key)
+        ## SEND CIPHERTEXT BACK TO SERVER
+    else:
+        raise Exception("Signature verification failed")
     
     # SERVER
-    server_key = derive_server_key(dk, ct)
+    server_key = decapsulate_key(decap_key, ciphertext)
+
 
     print(client_key == server_key)
