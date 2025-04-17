fd24c3b626ea0f3328f77178b506761a11a38fa4
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-04-17 18:58:52 +1000

    Upload Python files

--- main.py
+++
@@ -1,7 +0,0 @@
-from kyber_py.ml_kem import ML_KEM_1024
-
-if __name__ == "__main__":
-    ek, dk = ML_KEM_1024.keygen()
-    key, ct = ML_KEM_1024.encaps(ek)
-    _key = ML_KEM_1024.decaps(dk, ct)
-    print(key.hex())
