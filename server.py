816536b2daa0e2e9236893f2d4539a9415cd29fd
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-04-18 12:12:45 +1000

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,13 @@ import quic_handler
 DSA_PUB_KEY, DSA_SEC_KEY = crypto_utils.generate_dsa_keys()
 
 # Await incoming connection
+
+
 # Establish KEM key pair
 encap_key, decap_key = crypto_utils.generate_kem_keys()
 sig = crypto_utils.create_signature(DSA_SEC_KEY, encap_key)
 
 # Send DSA public key and encapsulation key to client
+
+
+# Await client response (containing ciphertext)
